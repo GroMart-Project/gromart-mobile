29a63088578bb60530bc11aceaa9bd0d1f89be84
Samuel Sackey <smlscky@gmail.com>
2022-04-01 23:37:48 +0000

    Add onboard footer

--- src/screens/OnboardingScreen.js
+++ src/screens/OnboardingScreen.js
@@ -1,8 +1,17 @@
-import { StyleSheet, SafeAreaView, FlatList, Dimensions } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
 
 import SlideData from "../data/SlideData";
 import Slide from "../components/Slide";
+import { COLORS } from "../data/Constants";
 
 const { width, height } = Dimensions.get("window");
 
@@ -17,9 +26,33 @@ export default function OnboardingScreen() {
         horizontal
         pagingEnabled
       />
+      <Footer />
     </SafeAreaView>
   );
 }
+
+const Footer = () => {
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+
+  return (
+    <View style={styles.footer}>
+      <View style={styles.indicatorContainer}>
+        {SlideData.map((_, index) => (
+          <View
+            key={index}
+            style={[
+              styles.indicator,
+              currentSlideIndex == index && { backgroundColor: COLORS.primary },
+            ]}
+          />
+        ))}
+      </View>
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.btnText}>Get Started</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -27,5 +60,36 @@ const styles = StyleSheet.create({
   },
   listContainer: {
     height: height * 0.75,
+  },
+  footer: {
+    height: height * 0.25,
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+  },
+  indicatorContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  indicator: {
+    height: 15,
+    width: 15,
+    borderColor: COLORS.primary,
+    borderWidth: 2,
+    borderRadius: 100,
+    marginHorizontal: 10,
+  },
+  btn: {
+    backgroundColor: COLORS.primary,
+    height: 70,
+    marginBottom: 20,
+    borderRadius: 40,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btnText: {
+    fontSize: 24,
+    fontWeight: "700",
+    color: "white",
   },
 });
