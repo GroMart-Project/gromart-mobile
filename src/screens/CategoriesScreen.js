f5c9e4fc96338f2525cdfa0a60e8e671d154bcef
Samuel Sackey <smlscky@gmail.com>
2022-04-30 13:50:47 +0000

    Setup data fetch and filtering

--- src/screens/CategoriesScreen.js
+++ src/screens/CategoriesScreen.js
@@ -1,7 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useLayoutEffect } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import HeaderStyles from "../components/utilities/HeaderStyles";
 import { COLORS } from "../data/Constants";
+
+//Firebase imports//
+import {
+  fetchCategoriesData,
+  fetchProductsData,
+} from "../utilities/firestoreQueries";
+import ProductBox from "../components/ProductBox";
 
 export default function CategoriesScreen({ navigation }) {
   // Header Styling//
@@ -10,16 +23,124 @@ export default function CategoriesScreen({ navigation }) {
   }, [navigation]);
   //Header Styling Ends//
 
+  //fetch data for categories//
+  const [categoriesData, setCategoriesData] = useState();
+
+  useEffect(() => {
+    fetchCategoriesData(setCategoriesData).catch((error) => console.log(error));
+  }, []);
+
+  //fetch ends//
+
+  //fetch data for products//
+  const [productsData, setProductsData] = useState([]);
+
+  useEffect(() => {
+    fetchProductsData(setProductsData)
+      .then(console.log(productsData))
+      .catch((error) => console.log(error));
+  }, []);
+
+  //fetch ends//
+
+  //State for active category//
+  const [activeCategory, setActiveCategory] = useState(0);
+  //state ends//
+
+  // Filter Products//
+  const filteredProducts = productsData.filter(
+    (product) => product.category == categoriesData[activeCategory].name
+  );
+  //Filter Products End//
+
   return (
     <View style={styles.container}>
-      <Text>CategoriesScreen</Text>
+      {/* Categories List */}
+      <View>
+        <FlatList
+          data={categoriesData}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item, index }) => (
+            <CategoriesButton
+              name={item.name}
+              activeCategory={activeCategory}
+              setActiveCategory={setActiveCategory}
+              index={index}
+            ></CategoriesButton>
+          )}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          style={styles.categoriesList}
+        />
+      </View>
+      {/* Categories List ends*/}
+
+      {/* Bottom Section */}
+      <View style={{ margin: 10 }}>
+        <FlatList
+          data={filteredProducts}
+          keyExtractor={(item) => item.id}
+          numColumns={2}
+          columnWrapperStyle={{
+            justifyContent: "space-between",
+            marginBottom: 10,
+          }}
+          renderItem={({ item }) => <ProductBox product={item} />}
+        />
+      </View>
+      {/* Bottom Section End */}
     </View>
   );
 }
+
+//Categories List component//
+const CategoriesButton = ({
+  name,
+  activeCategory,
+  setActiveCategory,
+  index,
+}) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.5}
+      style={[
+        styles.btnBox,
+        {
+          backgroundColor: activeCategory == index ? COLORS.primary : "white",
+          borderColor: activeCategory == index ? "black" : COLORS.box,
+        },
+      ]}
+      onPress={() => setActiveCategory(index)}
+    >
+      <Text
+        style={[
+          styles.btnText,
+          { color: activeCategory == index ? "white" : COLORS.text },
+        ]}
+      >
+        {name}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+//Components ends//
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.background,
+  },
+  categoriesList: {
+    marginVertical: 20,
+  },
+  btnBox: {
+    paddingHorizontal: 15,
+    paddingVertical: 7.5,
+    marginHorizontal: 7.5,
+    borderWidth: 1,
+    borderRadius: 10,
+  },
+  btnText: {
+    fontSize: 16,
   },
 });
