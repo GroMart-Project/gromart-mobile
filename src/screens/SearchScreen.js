3f35fe2f31e11a55552601c32fb7bdba2624c465
Samuel Sackey <smlscky@gmail.com>
2022-04-30 17:04:32 +0000

    Add searchbox

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,7 +1,8 @@
 import { StyleSheet, Text, View } from "react-native";
-import React, { useLayoutEffect } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import HeaderStyles from "../components/utilities/HeaderStyles";
 import { COLORS } from "../data/Constants";
+import { Searchbar } from "react-native-paper";
 
 export default function SearchScreen({ navigation }) {
   // Header Styling//
@@ -10,8 +11,24 @@ export default function SearchScreen({ navigation }) {
   }, [navigation]);
   //Header Styling Ends//
 
+  //State for searched products//
+  const [searchedProducts, setSearchedProducts] = useState();
+  //State ends//
+
+  //Function for searching//
+  const Search = async (searchKeyword) => {};
+  //function ends//
+
   return (
     <View style={styles.container}>
+      <View style={{ backgroundColor: "white" }}>
+        <Searchbar
+          placeholder="Search"
+          style={{ margin: 10 }}
+          theme={{ roundness: 10 }}
+        />
+        <View></View>
+      </View>
       <Text>SearchScreen</Text>
     </View>
   );
