b352095ccc1d67e19f4c66d2506648483174d65a
Samuel Sackey <smlscky@gmail.com>
2022-05-02 02:23:06 +0000

    Setup loading for search result screen

--- src/screens/SearchResultScreen.js
+++ src/screens/SearchResultScreen.js
@@ -1,4 +1,10 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import React, { useEffect, useLayoutEffect, useState } from "react";
 import HeaderStyles from "../components/utilities/HeaderStyles";
 import { COLORS } from "../data/Constants";
@@ -34,25 +40,49 @@ export default function SearchResultScreen({ navigation, route }) {
         (product.title.toLowerCase().includes(searchKey.toLowerCase()) ||
           product.category.toLowerCase().includes(searchKey.toLowerCase()))
     );
-    return setSearchedProducts(filteredProducts);
+    setSearchedProducts(filteredProducts);
   }, [productsData]);
-
   //filter ends//
 
+  //loading state and listener//
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = () => {
+      productsData && setLoaded(true);
+    };
+    return unsubscribe;
+  }, [productsData]);
+  //state ends//
+
+  if (!loaded) {
+    return (
+      <View style={[styles.container, { justifyContent: "center" }]}>
+        <ActivityIndicator size={50} animating={true} color={COLORS.primary} />
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
-      <View style={{ margin: 10 }}>
-        <FlatList
-          data={searchedProducts}
-          keyExtractor={(item) => item.id}
-          numColumns={2}
-          columnWrapperStyle={{
-            justifyContent: "space-between",
-            marginBottom: 10,
-          }}
-          renderItem={({ item }) => <ProductBox product={item} />}
-        />
-      </View>
+      {searchedProducts?.length == 0 ? (
+        <View style={styles.noResultContainer}>
+          <Text style={styles.noResultText}>No Results Found</Text>
+        </View>
+      ) : (
+        <View style={{ margin: 10 }}>
+          <FlatList
+            data={searchedProducts}
+            keyExtractor={(item) => item.id}
+            numColumns={2}
+            columnWrapperStyle={{
+              justifyContent: "space-between",
+              marginBottom: 10,
+            }}
+            renderItem={({ item }) => <ProductBox product={item} />}
+            showsVerticalScrollIndicator={false}
+          />
+        </View>
+      )}
     </View>
   );
 }
@@ -61,5 +91,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.background,
+  },
+  noResultContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  noResultText: {
+    color: COLORS.text,
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
