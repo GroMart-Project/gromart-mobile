592a05acacb3b179ab9b41cdcd97236fe9f804dc
Samuel Sackey <smlscky@gmail.com>
2022-05-03 01:51:35 +0000

    Add products to wishlist screen

--- src/screens/profile/WishlistScreen.js
+++ src/screens/profile/WishlistScreen.js
@@ -1,7 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
-import React, { useLayoutEffect } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import HeaderStyles from "../../components/utilities/HeaderStyles";
+import VerticalProductList from "../../components/VerticalProductList";
 import { COLORS } from "../../data/Constants";
+
+//firebase imports
+import {
+  fetchProductsData,
+  fetchWishlistData,
+} from "../../utilities/firestoreQueries";
 
 export default function WishlistScreen({ navigation }) {
   // Header Styling//
@@ -10,9 +17,44 @@ export default function WishlistScreen({ navigation }) {
   }, [navigation]);
   //Header Styling Ends//
 
+  //fetch data for products//
+  const [productsData, setProductsData] = useState([]);
+
+  useEffect(() => {
+    fetchProductsData(setProductsData).catch((error) => console.log(error));
+  }, []);
+  //fetch ends//
+
+  //fetch wishlist data realtime//
+  const [wishlistData, setWishlistData] = useState();
+
+  useEffect(() => {
+    const unsubscribe = fetchWishlistData(setWishlistData);
+    return unsubscribe;
+  }, []);
+  //fetch end//
+
+  //Filter products using search key//
+  const [wishlistProducts, setWishlistProducts] = useState();
+
+  useEffect(() => {
+    const filteredProducts = productsData?.filter(
+      (product) => wishlistData && wishlistData?.includes(product.id)
+    );
+
+    return setWishlistProducts(filteredProducts);
+  }, [productsData, wishlistData]);
+  //filter ends//
+
   return (
     <View style={styles.container}>
-      <Text>WishlistScreen</Text>
+      {wishlistProducts?.length == 0 ? (
+        <View style={styles.noResultContainer}>
+          <Text style={styles.noResultText}>No Items In Your Wishlist</Text>
+        </View>
+      ) : (
+        <VerticalProductList data={wishlistProducts} />
+      )}
     </View>
   );
 }
@@ -21,5 +63,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.background,
+  },
+  noResultContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  noResultText: {
+    color: COLORS.text,
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
