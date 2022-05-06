8e1a77bceab2c0207835b67ffb5a2cdc6195000d
Samuel Sackey <smlscky@gmail.com>
2022-05-06 02:27:31 +0000

    Setup async storage and remove from cart

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const initialState = {
   cartItems: [],
@@ -10,6 +11,9 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
+    loadProducts(state, action) {
+      state.cartItems = action.payload;
+    },
     addToCart(state, action) {
       const itemIndex = state.cartItems.findIndex(
         (item) => item.id == action.payload.id
@@ -20,10 +24,19 @@ const cartSlice = createSlice({
         const tempProduct = { ...action.payload, cartQuantity: 1 };
         state.cartItems.push(tempProduct);
       }
+
+      AsyncStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+    },
+    removeFromCart(state, action) {
+      const nextCartItems = state.cartItems.filter(
+        (cartItem) => cartItem.id !== action.payload.id
+      );
+
+      state.cartItems = nextCartItems;
     },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { loadProducts, addToCart, removeFromCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
